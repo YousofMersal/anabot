34826fbedf2daf3d036ffbcdcbe0b99f6cedccc9
Yousof Mersal <yousofmersal@gmail.com>
2021-08-27 17:07:02 +0200

    remove diesel files

--- migrations/2021-08-19-154901_create_timers/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE timers (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    body TEXT,
-    recurring BOOLEAN NOT NULL DEFAULT 'f',
-    raid_lead VARCHAR,
-    time TEXT NOT NULL
-)
