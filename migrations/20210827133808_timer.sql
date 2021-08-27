d7a3be1daf74e2caaf5998d1d90c5170d7e4b2e2
Yousof Mersal <yousofmersal@gmail.com>
2021-08-27 17:08:18 +0200

    add sqlx migration

---
+++ migrations/20210827133808_timer.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS timers (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    body TEXT,
+    recurring BOOLEAN NOT NULL DEFAULT 'f',
+    raid_lead VARCHAR,
+    time TEXT NOT NULL
+)
